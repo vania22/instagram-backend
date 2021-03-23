0fb190e1178f5688e3ec3defe2ab3f49654aa886
Ivan Krupskyi <krupskiy111@gmail.com>
2021-03-23 00:19:00 +0000

    Added field resolvers for user

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,7 +1,10 @@
-import {Arg, Mutation, Resolver, Query, ObjectType, Field, Ctx, Authorized} from "type-graphql";
+import {Arg, Authorized, Ctx, Field, FieldResolver, Mutation, ObjectType, Query, Resolver, Root} from "type-graphql";
 import {User} from "../entities/User";
 import {IContext} from "../interfaces/IContext";
 import {login, registrate} from "../services/UserResolverServices";
+import {Post} from "../entities/Post";
+import {Comment} from "../entities/Comment";
+import {Like} from "../entities/Like";
 
 @ObjectType()
 export class LoginResponse {
@@ -33,5 +36,20 @@ export class UserResolver {
     async me(@Ctx() {user}: IContext): Promise<User> {
         if(!user) throw new Error('User not found')
         return user
+    }
+
+    @FieldResolver()
+    async posts(@Root() user: User): Promise<Post[] | []> {
+        return await Post.find({user})
+    }
+
+    @FieldResolver()
+    async comments(@Root() user: User): Promise<Comment[] | []> {
+        return await Comment.find({user})
+    }
+
+    @FieldResolver()
+    async likes(@Root() user: User): Promise<Like[] | []> {
+        return await Like.find({user})
     }
 }
